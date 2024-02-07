129014573a1ed2bc86d153012d0108c542281f52
Lilian SOLER <soler.lilian64@gmail.com>
2024-02-07 17:18:19 +0100

    :sparkles: Add signin
    Add passport and jwt library
    Add a method to sign token
    Add signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,40 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signin(dto: AuthDto) {
+    // find the user in the database
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    // if the user is not found, throw an error
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    // compare the password hashes
+    const match = await argon.verify(user.hashedPassword, dto.password);
+    // if the password hashes don't match, throw an error
+    if (!match) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+    return this.signToken(user.id, user.email);
+  }
 
   async signup(dto: AuthDto) {
+    // generate a hash of the password
     const hash = await argon.hash(dto.password);
-
+    // save the user in the database
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -33,7 +59,17 @@ export class AuthService {
     }
   }
 
-  signin(dto: AuthDto) {
-    return dto;
+  async signToken(
+    userId: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = { sub: userId, email };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+    return {
+      access_token: token,
+    };
   }
 }
