406d40597a249293417febfc56d49bae87717212
Lilian SOLER <soler.lilian64@gmail.com>
2024-02-14 13:26:42 +0100

    :construction: Added documentation for company CRUD operations
    TODO: Test the CRUD functionality and implement RBAC using existing guards and decorators

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -3,37 +3,106 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto, UpdateCompanyDto } from './dto';
+import { CompanyEntity } from './entities';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('company')
+@ApiResponse({
+  status: HttpStatus.BAD_REQUEST,
+  description: 'Bad request',
+})
+@ApiResponse({
+  status: HttpStatus.INTERNAL_SERVER_ERROR,
+  description: 'Internal server error',
+})
+@ApiBearerAuth()
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
+  @ApiOperation({ summary: 'Create company' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Company created',
+    type: CompanyEntity,
+  })
+  @HttpCode(HttpStatus.CREATED)
   @Post()
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companyService.create(createCompanyDto);
   }
 
+  @ApiOperation({ summary: 'Get all companies' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Companies list',
+    type: [CompanyEntity],
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Companies not found',
+  })
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll() {
     return this.companyService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get company by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Company found',
+    type: CompanyEntity,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Company not found',
+  })
+  @HttpCode(HttpStatus.OK)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.companyService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update company by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Company updated',
+    type: CompanyEntity,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Company not found',
+  })
+  @HttpCode(HttpStatus.OK)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companyService.update(id, updateCompanyDto);
   }
 
+  @ApiOperation({ summary: 'Delete company by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Company deleted',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Company not found',
+  })
+  @HttpCode(HttpStatus.OK)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.companyService.remove(id);
--- src/docs/options.ts
+++ src/docs/options.ts
@@ -14,6 +14,7 @@ const config = new DocumentBuilder()
   .addTag('auth')
   .addTag('users')
   .addTag('projects')
+  .addTag('company')
 
   .addBearerAuth()
 
