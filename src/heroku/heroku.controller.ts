9233211bb91f5b696204958cf15cc67c38a90d7f
Lilian SOLER <soler.lilian64@gmail.com>
2024-02-24 10:59:43 +0100

    :zap: Enhanced invoice management including updates
    Improved messaging to provide clearer feedback
    Proper HTTP status codes to ensure better communication with clients
    Managing updates to existing invoices
    Implemented optimizations for handling invoices

--- src/heroku/heroku.controller.ts
+++ src/heroku/heroku.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { HerokuService } from './heroku.service';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
@@ -6,17 +13,20 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 export class HerokuController {
   constructor(private readonly herokuService: HerokuService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_NOON)
   @Post()
   create() {
     return this.herokuService.create();
   }
 
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll() {
     return this.herokuService.findAll();
   }
 
+  @HttpCode(HttpStatus.OK)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.herokuService.findOne(id);
