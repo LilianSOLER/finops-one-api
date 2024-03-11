c566fa3cfcbd614f13b866c853fcf1878016f5fb
Lilian SOLER <soler.lilian64@gmail.com>
2024-03-11 11:08:52 +0100

    :bug: Fix issues in migrations

--- prisma/migrations/20240208110639_update_aws_user/migration.sql
+++ prisma/migrations/20240208110639_update_aws_user/migration.sql
@@ -7,15 +7,3 @@
 -- AlterTable
 ALTER TABLE "Aws_credentials" DROP COLUMN "createAt";
 
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'USER',
-    "hash" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
